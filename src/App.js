eacf5f8ea26ace20f81cefbf1db736fac92fa46d
elina <elinaisabekova857@gmail.com>
2022-10-17 17:56:10 +0300

    nnn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react"
+
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const Plus = () => {
+    setCount(count + 1);
+  };
+
+  const Minus = () => {
+    if (count > 0){
+      setCount(count - 1);
+    };
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter">
+       <h1>Counter in React</h1>
+       <div className="count">
+        <div>{count}</div>
+        <button onClick={Plus}>increase</button>
+        <button onClick={Minus}>decrease</button>
+       </div>
     </div>
   );
 }
